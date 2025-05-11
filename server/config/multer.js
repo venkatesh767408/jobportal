47aec6537f904943c6b586922ac9ad5f3189e4db
CHAMRTHI VENKATESH <160413645+venkatesh767408@users.noreply.github.com>
2025-05-11 09:47:36 +0530

    Update multer.js

--- server/config/multer.js
+++ server/config/multer.js
@@ -1,23 +1,7 @@
+// server/config/multer.js
 import multer from 'multer';
-import path from 'path';
-import fs from 'fs';
 
-// Ensure uploads folder exists
-const uploadDir = path.join('uploads');
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
-
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, uploadDir);
-  },
-  filename: (req, file, cb) => {
-    const ext = path.extname(file.originalname);
-    const uniqueName = `${Date.now()}-${Math.round(Math.random() * 1E9)}${ext}`;
-    cb(null, uniqueName);
-  }
-});
+const storage = multer.memoryStorage(); // Store uploaded files in memory (RAM)
 
 const upload = multer({ storage });
 
